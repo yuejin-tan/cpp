a688ad89bb182fa81e5675988f41036d3a96a854
yuejinTan <498339337@qq.com>
2024-09-22 22:03:32 +0800

    练习排序算法

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,67 @@
 
 #include <array>
 
-const int ARR_SIZE = 32;
+const int ARR_SIZE = 64;
 
 int a0[ARR_SIZE];
 int a1[ARR_SIZE];
 int a2[ARR_SIZE];
+
+void BubbleSort(int arr[], int len)
+{
+    for (int i = 0;i < len;i++)
+    {
+        for (int j = 0;j < len - i - 1;j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void SelSort(int arr[], int len)
+{
+    for (int i = 0;i < len;i++)
+    {
+        int minIdx = i;
+        int idxVal = arr[minIdx];
+        for (int j = i + 1;j < len;j++)
+        {
+            if (arr[j] < arr[minIdx])
+            {
+                minIdx = j;
+                idxVal = arr[j];
+            }
+        }
+
+        if (minIdx != i)
+        {
+            arr[minIdx] = arr[i];
+            arr[i] = idxVal;
+        }
+    }
+}
+
+void InsertSort(int arr[], int len)
+{
+    for (int i = 1;i < len;i++)
+    {
+        if (arr[i] < arr[i - 1])
+        {
+            int temp = arr[i];
+            int j;
+            for (j = i - 1;(j >= 0) && (arr[j] > temp);j--)
+            {
+                arr[j + 1] = arr[j];
+            }
+            arr[j + 1] = temp;
+        }
+    }
+}
 
 int main(int, char**) {
     srand(time(nullptr));
@@ -20,6 +76,9 @@ int main(int, char**) {
     }
 
     qsort(a1, ARR_SIZE, sizeof(int), [](const void* x, const void* y) {return *(int*)x - *(int*)y;});
+
+    // test
+    InsertSort(a2, ARR_SIZE);
 
     bool isNeq = false;
 
@@ -36,6 +95,10 @@ int main(int, char**) {
     if (isNeq)
     {
         printf("---------------failed!!--------------\n");
+    }
+    else
+    {
+        printf("---------------pass!!--------------\n");
     }
 
     return 0;
